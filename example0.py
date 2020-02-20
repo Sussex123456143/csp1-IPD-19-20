95fa2b573f29e2384b7ce9f75ddffe17aa6ab2a3
Sussex123456143 <archithsaisurya@gmail.com>
2020-02-20 07:30:19 +0000

    Added some strategies

--- example0.py
+++ example0.py
@@ -1,26 +1,14 @@
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
 
-team_name = 'E0'
-strategy_name = 'Collude'
-strategy_description = 'Always collude.'
-    
-def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
-    
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
-    '''
-    
-    # This player always colludes.
-    return 'c'
-    
+    ##### Each team's file must define four tokens:# team_name: a string# strategy_name: a string# strategy_description: a string# move: A function that returns 'c' or 'b'####
+team_name = 'Rest Stop @ I580'
+strategy_name = 'Collude Unless '
+strategy_description = 'Always collude unless faced with a person who betrays more then they.' 
+def move(my_history, their_history, my_score, their_score): 
+  """Make my move based on the history with this player. observes the opponents history. If they collude more then they betray, I collude. Otherwise, I betray Returns 'c' or 'b' for collude or betray.""" 
+  trust =0 
+  distrust=0 
+  for hist in their_history: 
+    if hist == "c": trust += 1 
+    if hist == "b": distrust += 1 
+  if trust > distrust: return "c" 
+  else: return "b" 
